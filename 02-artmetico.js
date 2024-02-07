cab289957c118ca61df9ece5304ed3d595527ef6
Gabriela <gabrielasiqueiracaputti@gmail.com>
2024-02-07 11:26:09 -0300

    javascript - basico

---
+++ 02-artmetico.js
@@ -0,0 +1,28 @@
+//utilizando operadores aritmeticos com JavaScript
+
+console.log("A soma entre 8 e 7 é:", 8 + 7);
+
+//efetuar a soma de dois numeros informados por você (var nome_variavel = 12)
+
+var x = 4;
+var y = 6;
+
+console.log("A soma entre", x, "e", y, "é:", x + y);
+
+var soma = x + y;
+var subtracao = x - y;
+var multiplicacao = x * y;
+var divisao = x / y;
+
+console.log("Soma = ", soma);
+console.log("Subtracao = ", subtracao);
+console.log("Multiplicacao = ", multiplicacao);
+console.log("Divisao = ", divisao);
+
+//Regras para nomear identificadores (var, const ou let)
+//Podem começar com A-Z, a-z, _ ou $
+
+var MinhaVariavel = 10;
+var outraVariavel = 20;
+const _minhaConstante = 3.1514;
+var $minhaVariavel = 11;
