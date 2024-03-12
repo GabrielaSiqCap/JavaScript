7436d18e969b4c582218f04896bea257cf194b79
Gabriela <gabrielasiqueiracaputti@gmail.com>
2024-03-12 16:33:27 -0300

    objeto

---
+++ 19-objeto.js
@@ -0,0 +1,149 @@
+// Definindo uma classe Pessoa
+class Pessoa {
+  constructor(pNome, pIdade) {
+    this.nome = pNome;
+    this.idade = pIdade;
+  }
+  apresentarSe() {
+    console.log(`Olá, meu nome é ${this.nome} e eu tenho ${this.idade} anos.`);
+  }
+  envelhecer() {
+    this.idade++;
+  }
+}
+
+// Criando objetos do "tipo" Pessoa
+var pessoa1 = new Pessoa("Talda Dienifer", 52);
+var p2 = new Pessoa("Taldo Pedrin", 18);
+
+pessoa1.apresentarSe();
+p2.apresentarSe();
+
+pessoa1.envelhecer();
+pessoa1.apresentarSe();
+
+p2.envelhecer();
+p2.apresentarSe();
+
+console.log("------------------Abstração------------------");
+//Abstração: abstrair características importantes e descartar detalhes irrelevantes!
+
+class Pet {
+  //toda classe precisa iniciarse com maiusculo para não ser considerada variavel
+  constructor(nome, idade, tipo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+  }
+  alimentar() {
+    console.log(`${this.nome} foi alimentado!`);
+  }
+}
+
+const meuPet = new Pet("Rex", 2, "Chachorro");
+
+console.log(
+  `Meu pet é um ${meuPet.tipo}, chamado ${meuPet.nome} e tem ${meuPet.idade} anos!`
+);
+
+meuPet.alimentar();
+
+console.log("----------------------------Herença----------------------------");
+class Animal {
+  constructor(nome, idade, tipoAlimentacao) {
+    this.nome = nome;
+    this.idade = idade;
+    this.tipoAlimentacao = tipoAlimentacao;
+  }
+
+  alimentar() {
+    console.log(`O animal ${this.nome} foi alimentado!`);
+  }
+
+  emitirSom() {
+    console.log(`Barulho padrão do animal`);
+  }
+}
+
+class Catioro extends Animal {
+  alimentar() {
+    console.log(`O cachorro ${this.nome} foi alimentado!`);
+  }
+  emitirSom() {
+    console.log(`🐶Au au!🐶`);
+  }
+}
+const pet = new Animal("Carlos", 10, "Comida espacial");
+const meuCachorro = new Catioro("Rex", 3, "Ração");
+
+pet.alimentar();
+pet.emitirSom();
+console.log("");
+meuCachorro.alimentar();
+meuCachorro.emitirSom();
+
+console.log("-------------------Encapsulamento-------------------");
+
+class Petzinho {
+  constructor(nome, idade, tipo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+  }
+
+  getNome() {
+    return this.nome;
+  }
+
+  setNome(novoNome) {
+    this.nome = novoNome;
+  }
+}
+
+const novoPetizinho = new Petzinho("Bob", 3, "Cachorro");
+
+console.log(novoPetizinho.getNome()); //"Bob"
+novoPetizinho.setNome("Rex");
+console.log(novoPetizinho.getNome()); //"Rex"
+
+console.log("-----------------------Polimorfismo-----------------------");
+class Animalzao {
+  constructor(nome) {
+    this.nome = nome;
+  }
+
+  fazerBarulho() {
+    console.log("O animal faz um barulho genérico.");
+  }
+}
+
+class Cachorro extends Animalzao {
+  constructor(nome) {
+    super(nome);
+  }
+
+  fazerBarulho() {
+    console.log("O cachorro late!");
+  }
+}
+
+class Gato extends Animalzao {
+  constructor(nome) {
+    super(nome);
+  }
+
+  fazerBarulho() {
+    console.log("O gato mia!");
+  }
+}
+
+// Exemplo de uso
+const animais = [
+  new Cachorro("Rex"),
+  new Gato("Mia"),
+  new Animalzao("Bob"), // Animal genérico
+];
+
+for (const animal of animais) {
+  animal.fazerBarulho();
+}
